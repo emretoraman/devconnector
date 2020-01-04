b02e6531a9b53402e145764a171523695e0978e6
Emre Toraman <emretoraman@gmail.com>
2020-01-04 04:28:53 +0200

    Register & Login component, Redux store, Alert reducer & component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Login = () => (
-    <Fragment>Login</Fragment>
-);
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        console.log(formData);
+    }
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Login To Your Account</p>
+            <form className="form" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email" required value={email} onChange={onChange} />
+                </div>
+                <div className="form-group">
+                    <input type="password" placeholder="Password" name="password" minLength="6" value={password} onChange={onChange} />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </Fragment>
+    );
+};
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,60 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { setAlert } from '../../actions/alert';
 
-const Register = () => (
-    <Fragment>Register</Fragment>
-);
+const Register = ({ setAlert }) => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
 
-export default Register;
+    const { name, email, password, password2 } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if (password !== password2) {
+            setAlert('Passwords do not match', 'danger');
+            return;
+        }
+
+        console.log(formData);
+    }
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input type="text" placeholder="Name" name="name" required value={name} onChange={onChange} />
+                </div>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email" required value={email} onChange={onChange} />
+                    <small className="form-text">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                </div>
+                <div className="form-group">
+                    <input type="password" placeholder="Password" name="password" minLength="6" value={password} onChange={onChange} />
+                </div>
+                <div className="form-group">
+                    <input type="password" placeholder="Confirm Password" name="password2" minLength="6" value={password2} onChange={onChange} />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
+    );
+};
+
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
